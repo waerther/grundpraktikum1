7d07f5719b1e6fa2388cb687b3e092709437d942
Erich Wagner <erich.wagner@tu-dortmund.de>
2021-12-20 02:05:01 +0100

    v355 die Fünfte fml

--- v355/plot.py
+++ v355/plot.py
@@ -1,21 +1,31 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from uncertainties import ufloat
+from uncertainties import unumpy as unp
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+frequenz = pd.read_csv('md_frequenz.csv')
+frequenz = frequenz.iloc[:,:]
+np.savetxt('frequenz.txt', frequenz.values, header='nu^+ nu^-', fmt='%.3f')
+ck, nu_p, n_m = np.genfromtxt('frequenz.txt', unpack=True, skip_header=1)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+def nu_minus(l, c, ck):
+    return (1/(2*np.pi)) * ( l * ( (1/c) + (2/ck) )**(-1) )**(-1/2)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+L = 0.023954
+C = 0.7932e-9
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+x = np.linspace(0.997, 13, 120)
+y = nu_minus(L, C, x*10**-9)
+
+plt.plot(x, y*10**-3, '-r', label = 'Theoriewerte')
+plt.plot(ck, n_m, 'xb', label = 'Messwerte')
+plt.xlabel(r'$C_{K} \,/\, \mathrm{nF}$')
+plt.ylabel(r'$\nu^- \,/\, \mathrm{kHz}$')
+plt.legend(loc="best")
+plt.xlim(0, 12.5)
+plt.ylim(35, 58)
+plt.grid(True)
+
+plt.savefig('build/plot1.pdf', bbox_inches = "tight")
+plt.clf() 
