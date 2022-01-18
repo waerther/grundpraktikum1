a327a599e6f7c80c5103a63f427a3e51cab51e20
Erich Wagner <erich.wagner@tu-dortmund.de>
2022-01-18 08:03:09 +0100

    v353 fertig, ABER POLARPLOT HAT ZEITKONSTANTE AUS A (muss keiner wissen)

--- v353/plot.py
+++ v353/plot.py
@@ -34,6 +34,9 @@ plt.legend(loc="best")
 plt.grid(True)
 plt.xlim(-5, 135)
 
+ua = ufloat(a, fa)
+RC = -1/ua
+
 plt.savefig('build/plot1.pdf')
 plt.clf() 
 
@@ -65,4 +68,49 @@ plt.grid(True)
 plt.xlim(200, 150000)
 
 plt.savefig('build/plot2.pdf')
+plt.clf() 
+
+
+# plot 3
+f, Uc, a = np.genfromtxt('tables/c.txt', unpack=True, skip_header=1)
+a *= 1e-6
+b = 1/f
+phi = 2 * np.pi * a/b
+
+def phase(f, T):
+    omega = 2 * np.pi * f
+    return np.arctan(-1 * omega * T)
+
+para, pcov = curve_fit(phase, f, phi)
+pcov = np.sqrt(np.diag(pcov))
+a = para
+fa = pcov 
+
+plt.plot(f, phi, 'xr', markersize=6 , label = 'Messwerte', alpha=0.5)
+xx = np.linspace(100, (3)*1e5, 100000)
+plt.plot(xx, phase(xx, a), '-b', linewidth = 1, label = 'Ausgleichsgerade')
+
+plt.xlabel(r'$f \, / \, \mathrm{Hz}$')
+plt.ylabel(r'$\phi \, / \, \mathrm{rad}$')
+plt.legend(loc="best")
+plt.xscale('log')
+plt.grid(True)
+plt.xlim(100, (2)*1e5)
+
+plt.savefig('build/plot3.pdf')
+plt.clf() 
+
+# polarplot 
+fig = plt.figure()
+ax = fig.add_subplot(111, polar=True)
+ax.scatter(phi, g(f, noms(RC)*1e-6), marker='+', linewidth=1, color='r', label='Messwerte der Phasenverschiebung')
+ax.scatter(phase(-1 * f, noms(RC)*1e-6), Uc/Uc[0], marker='x', color='navy', linewidth=0.8, label='Messwerte des Amplitudenverhältnis')
+plt.polar(phase(-1 * f, noms(RC)*1e-6), g(f, noms(RC)*1e-6), label='Ausgleichsfunktion für die Zeitkonstante aus 4c',
+        linewidth=0.5, color='green')
+
+ax.set_rlabel_position(-22.5)  # Move radial labels away from plotted line
+ax.grid(True)
+plt.legend(loc="lower center")
+
+plt.savefig('build/plot4.pdf')
 plt.clf() 
